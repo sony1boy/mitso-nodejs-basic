7ff13cc53616b171a32477efd8b2a04c69df52d7
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:17:47 +0300

    add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,44 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+import { fileURLToPath } from 'url';
 
-await calculateHash();
+
+async function calcHash() {
+   
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const sourceFolder = path.join(__dirname, 'files');
+    const oldFilename = 'fileToCalculateHashFor.txt';
+    const sourceFilePath = path.join(sourceFolder, oldFilename);
+
+    try {
+        await fs.promises.access(sourceFilePath);
+
+        const fileStream = fs.createReadStream(sourceFilePath);
+
+        const hash = crypto.createHash('sha256');
+
+        fileStream.on('data', (chunk) => {
+            hash.update(chunk);
+        });
+
+        fileStream.on('end', () => {
+            const digest = hash.digest('hex'); 
+            console.log('SHA256 Hash:', digest);
+        });
+
+        fileStream.on('error', (error) => {
+            console.error('Error reading file:', error);
+        });
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('File not found:', sourceFilePath);
+        } else {
+            console.error('Error accessing file:', error);
+        }
+    }
+}
+
+calcHash();
