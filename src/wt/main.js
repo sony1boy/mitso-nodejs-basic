9ea437b4ce19b9ee34981688620d7dcb093b951a
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:48:02 +0300

    add main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,46 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import * as os from 'os';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker, isMainThread } from 'worker_threads';
 
-await performCalculations();
+if (isMainThread) {
+    function createWorkers() {
+        const numCores = os.cpus().length;
+        const results = [];
+        let workersFinished = 0;
+
+        function handleMessage(workerIndex, message) {
+            results[workerIndex] = message;
+            workersFinished++;
+
+            if (workersFinished === numCores) {
+                console.log('All workers have finished their tasks.');
+                console.log('Results:', results);
+            }
+        }
+
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+
+        for (let i = 0; i < numCores; i++) {
+            const workerPath = path.join(__dirname, '..', 'wt', 'worker.js');
+            const worker = new Worker(workerPath, { workerData: 10 + i });
+            worker.on('message', handleMessage.bind(null, i));
+
+            worker.on('error', (error) => {
+                results[i] = { status: 'error', data: null };
+                console.error(`Worker ${i} error:`, error);
+                workersFinished++;
+
+                if (workersFinished === numCores) {
+                    console.log('All workers have finished their tasks.');
+                    console.log('Results:', results);
+                }
+            });
+        }
+    }
+
+    createWorkers();
+} else {
+    console.error('This script should be run as main.');
+}
