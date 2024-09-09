847821fc8df7be8b36e80ed234ebc3d6e012019d
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:13:26 +0300

    add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+  const folderPath = path.resolve(__dirname, 'files'); 
+  const filename = 'fileToRemove.txt';
+  const filePath = path.join(folderPath, filename);
+
+  try {
+
+    await fs.access(filePath, fs.constants.F_OK);
+
+
+    await fs.unlink(filePath);
+    console.log('File deleted successfully');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.error('FS operation failed: File does not exist');
+    } else {
+      console.error('FS operation failed:', error.message);
+    }
+  }
 };
 
 await remove();
