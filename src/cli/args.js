a5272c31f670423ad243bdc2ffaffccf6c6d5fee
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:03:51 +0300

    create args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    if (args.length % 2 !== 0) {
+        console.error('Неверное количество аргументов');
+        return;
+    }
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].slice(2);
+        const value = args[i + 1];
+        console.log(`${propName} is ${value}`);
+    }
 };
 
 parseArgs();
