b5d13f203e63ddb1aeb7d558d5e474a3bdea0457
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:13:55 +0300

    add list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const folderPath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+
+    await fs.access(folderPath, fs.constants.F_OK);
+
+    const fileNames = await fs.readdir(folderPath);
+
+    console.log('File names in "files" folder:', fileNames);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.error('FS operation failed: Folder does not exist');
+    } else {
+      console.error('FS operation failed:', error.message);
+    }
+  }
 };
+
 
 await list();
