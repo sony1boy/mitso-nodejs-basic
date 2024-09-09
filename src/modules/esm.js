7e6ea5771bc262d8979ec65847c942d4e7561ab6
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:40:20 +0300

    add esm.js

--- src/modules/esm.js
+++ src/modules/esm.js
@@ -1,35 +1,52 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
+import { createServer as createServerHttp } from 'http';
+import { release, version } from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { promises as fs } from 'fs';
+
+// Получение абсолютного пути к текущему файлу и директории
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Формирование относительных путей к JSON-файлам
+const aJSONPath = path.join(__dirname, 'files', 'a.json');
+const bJSONPath = path.join(__dirname, 'files', 'b.json');
+
+// Функция для динамической загрузки JSON-файлов
+async function loadJSON(filePath) {
+    const data = await fs.readFile(filePath, 'utf8');
+    return JSON.parse(data);
+}
+
+// Загрузка JSON-файлов
+const aJSON = await loadJSON(aJSONPath);
+const bJSON = await loadJSON(bJSONPath);
+
+// Импорт скрипта c.js
+import './files/c.js';
 
 const random = Math.random();
 
 let unknownObject;
 
-const moduleCPath = path.join(__dirname, 'files', 'c.js');
-require(moduleCPath);
-
-const filePathA = path.join(__dirname, 'files', 'a.json');
-const filePathB = path.join(__dirname, 'files', 'b.json');
-
 if (random > 0.5) {
-    unknownObject = require(filePathA);
+    unknownObject = aJSON;
 } else {
-    unknownObject = require(filePathB);
+    unknownObject = bJSON;
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${import.meta.url}`);
+console.log(`Path to current directory is ${path.dirname(import.meta.url)}`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
-const PORT = 3000;
+const PORT = 3020;
 
 console.log(unknownObject);
 
@@ -38,7 +55,4 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
+export { myServer, unknownObject };
