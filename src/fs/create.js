80887405d3d199a84e7f41d253a02a7d54871589
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:12:53 +0300

    add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import fs from "fs/promises"
+import path from "path";
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const filePath = path.join(__dirname, "files", "files.txt");
+const CONTENT = "Hello ";
 const create = async () => {
-    // Write your code here 
+
+    try{ 
+        const files = await fs.readdir(path.join(__dirname, "files"));
+        if(files.includes("fresh.txt")) throw new Error ("FS operation failed ");
+
+        await fs.writeFile(filePath, CONTENT, {
+            flag:"wx", 
+        });
+
+        console.log("Данные записаны!");
+
+    } catch (err) {
+        console.log(err.message);
+    }
+
 };
+
+
 
 await create();
