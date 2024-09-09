e7f58dd6955017845d8c3c0e940fba8fed874857
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:11:58 +0300

    create copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+    try{
+        await fs.cp(`${path.resolve(__dirname, 'files')}`,`${path.resolve(__dirname, 'files_copy')}`,{
+            recursive : true,
+            force: false,
+            errorOnExist : true
+        });
+        console.log('Files have successfully been copied')
+    }
+    catch(err){
+        console.log('FS operation failed');
+    }
 };
 
 await copy();
