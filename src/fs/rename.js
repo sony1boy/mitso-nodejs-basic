60bb92cbe46bdbdeff99dae7c7a7ad7162ca20d9
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:15:34 +0300

    add rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,41 @@
-const rename = async () => {
-    // Write your code here 
-};
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
 
-await rename();
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+async function moveAndRenameFile() {
+
+  const sourceFolder = path.join(__dirname, 'files_copy');
+  const destinationFolder = path.join(__dirname, 'files_rename');
+  const oldFilename = 'wrongFilename.txt';
+  const newFilename = 'properFilename.md';
+
+  const sourceFilePath = path.join(sourceFolder, oldFilename);
+  const destinationFilePath = path.join(destinationFolder, newFilename);
+
+  try {
+    await fs.access(sourceFilePath);
+
+    try {
+      await fs.access(destinationFilePath);
+      console.error('FS operation failed: File already exists');
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        try {
+          await fs.rename(sourceFilePath, destinationFilePath);
+          console.log('File moved and renamed successfully');
+        } catch (err) {
+          console.error('FS operation failed:', err.message);
+        }
+      } else {
+        console.error('FS operation failed:', error.message);
+      }
+    }
+  } catch (error) {
+    console.error('FS operation failed:', error.message);
+  }
+}
+
+await moveAndRenameFile();
