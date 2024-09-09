c34800ec2910fc2305f1005c7d5efdc4c16fbc6c
sony1boy <pashaelizarov8@gmail.com>
2024-09-09 15:42:34 +0300

    add transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedChunk = chunk.toString().trim();
+            if (reversedChunk.toLowerCase() === 'exit') {
+                process.exit();
+            }
+            const reversedText = reversedChunk.split('').reverse().join('');
+            this.push(reversedText + '\n');
+            callback();
+        }
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
